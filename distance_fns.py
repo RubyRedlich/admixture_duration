cf717edf3f46ae4160ac715a2bffaf42b49e49bc
RubyRedlich <rubyredlich@comcast.net>
2026-04-09 16:24:23 -0400

    notes

--- distance_fns.py
+++ distance_fns.py
@@ -1,1 +1,9 @@
 # Functions for comparing the distances/resolvability between distributions of summary statistics from different models
+
+# Compute likelihood of pairwise coalescence counts given a demographic model
+
+# Compute likelihood ratio of models given pairwise coalescence counts
+
+# Numerically calculate the theoretical pairwise coalescence time distribution given a demographic model
+
+# Estimate the KL divergence (or TVD, etc.) between the theoretical pairwise coalescence time distributions
